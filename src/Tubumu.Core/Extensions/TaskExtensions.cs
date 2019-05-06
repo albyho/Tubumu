2ae44200fc71da788d7c6c4e1f75ddfd372145a4
alby <alby@foxmail.com>
2019-05-06 13:23:57 +0800

    新增 TaskExtensions.cs

---
+++ src/Tubumu.Core/Extensions/TaskExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Tubumu.Core.Extensions
+{
+    public static class TaskExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] // 造成编译器优化调用
+        public static void NoWarning(this Task task)
+        {
+
+        }
+    }
+}
