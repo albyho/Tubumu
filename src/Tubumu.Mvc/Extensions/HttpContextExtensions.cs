ef0886be188e8bc66feca30ade2f230df53e3098
alby <alby@foxmail.com>
2019-11-29 14:23:29 +0800

    新增 HttpContext 扩展方法 GetRealIp

---
+++ src/Tubumu.Mvc/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+
+namespace Tubumu.Mvc.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        public static IPAddress GetRealIp(this HttpContext context)
+        {
+            IPAddress ip;
+            var headers = context.Request.Headers;
+            if (!headers.ContainsKey("X-Forwarded-For") || !IPAddress.TryParse(headers["X-Forwarded-For"].ToString().Split(',')[0], out ip))
+            {
+                ip = context.Connection.RemoteIpAddress;
+            }
+            return ip;
+        }
+    }
+}
